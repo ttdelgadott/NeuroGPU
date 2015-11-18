8980540afd36c9fe0d053c470fbe24c778d26006
ugotnitined <nitin.sadras@gmail.com>
2015-11-18 14:49:59 -0800

    separate file for ctypes struct class definitions

--- UrapNeuron/extractModel.py
+++ UrapNeuron/extractModel.py
@@ -1,7 +1,10 @@
 import neuron as nrn
 import ctypes
-from cStringIO import StringIO
 import sys
+
+from cStringIO import StringIO
+from nrn_structs import *
+
 
 def nrn_dll(printpath=False):
     """Return a ctypes object corresponding to the NEURON library.
@@ -48,37 +51,25 @@ def nrn_dll_sym(name, type=None):
     else:
         return type.in_dll(dll, name)
 
-class NrnThread(ctypes.Structure):
-    _fields_ = [("_t", ctypes.c_double),
-                ("_dt", ctypes.c_double),
-                ("cj", ctypes.c_double),
-                ("tml", ctypes.c_void_p),
-                ("hcell", ctypes.c_int),
-                ("end", ctypes.c_int),
-                ("id", ctypes.c_int),
-                ("_stop_stepping", ctypes.c_int),
-                ("_actual_rhs", ctypes.POINTER(ctypes.c_double)),
-                ("_actual_d", ctypes.POINTER(ctypes.c_double)),
-                ("_actual_a", ctypes.POINTER(ctypes.c_double)),
-                ("_actual_b", ctypes.POINTER(ctypes.c_double)),
-                ("_actual_v", ctypes.POINTER(ctypes.c_double)),
-                ("_actual_area", ctypes.POINTER(ctypes.c_double)),
-                ("_v_parent_index", ctypes.POINTER(ctypes.c_int))]
 
-USE_CTYPES = False
+USE_CTYPES = True
 modelFile = "./runModel.hoc"
 nrn.h.load_file(1, modelFile)
 if USE_CTYPES:
     nrn_nthread = nrn_dll_sym('nrn_threads', ctypes.c_void_p)
     thread = ctypes.cast(nrn_nthread, ctypes.POINTER(NrnThread))
+    #FN_TopoList
     for i in range(thread.contents.end):
-        print i, thread.contents._v_parent_index[i]
+        print i, thread.contents._v_parent_index[i] 
+
 else:
-
     for s in nrn.h.allsec():
+        # FN_Topo 
         name = nrn.h.secname()
-
         topology =  [s.nseg, s.L, s.diam, s.Ra, s.cm, nrn.h.dt, nrn.h.st.delay, nrn.h.st.dur, nrn.h.st.amp, nrn.h.tfinal, s.v, nrn.h.area(.5), nrn.h.parent_connection(), nrn.h.n3d()]
 
-    topolist = nrn.h.MyTopology()
+    # FN_RecSites
+    for s in nrn.h.recSites:
+        print s.name()
+    #topolist = nrn.h.MyTopology()
     
