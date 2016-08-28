59fa48d5c977e6678e48df5f01f15eb03f8f6522
devloop0 <nathreya.master@gmail.com>
2016-08-28 14:05:47 -0700

    More progress on create_auxilliary_data_3.m.

--- UrapNeuron/MainenPy/create_auxilliary_data_3.py
+++ UrapNeuron/MainenPy/create_auxilliary_data_3.py
@@ -20,5 +20,22 @@ def creat_auxilliary_data_3(A, N, NSeg, Parent, Neuron, cmVec, FN_TopoList):
 		e[i + 1] = A[father_base[i], i]
 	f[N] = 0
 	ksx = get_parent_from_neuron(FN_Topolist)
-	# Everything up to line 28 translated
-	# TODO: Start from line 29
+	ks = [0]
+	ks.append(list(N + 1 - ksx[N + 2 - ksx[2:]]))
+	ks = np.array(ks)
+	aux = Aux()
+	aux.Ks = ks
+	father_base = ks[2:]
+	father = [father_base]
+	father.append(father_base.size + 2)
+	father.append(father_base.size + 2)
+	father = np.array(father)
+	f_idxs_x_dimens = np.ceil(np.log2(N))+ 3
+	f_idxs_x = np.zeroes((f_idxs_x_dimens, f_idxs_x_dimens))
+	for i in range(1, np.ceil(np.log2(N)) + 3 + 1):
+		cur_f = np.array(list(range(1, father.size + 1)))
+		for j in range(1, np.power(2, i - 1) + 1):
+			cur_f = father[cur_f]
+		f_idxs_x[i,:] = cur_f
+	# Everything up to line 41 translated
+	# TODO: Start from line 42
