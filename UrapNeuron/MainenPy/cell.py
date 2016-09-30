040a04825cc63a75f05dcf21c28118f51a85ca0d
devloop0 <nathreya.master@gmail.com>
2016-09-30 12:01:09 -0700

    Fixed cell_2_vec.

--- UrapNeuron/MainenPy/cell.py
+++ UrapNeuron/MainenPy/cell.py
@@ -9,12 +9,22 @@ def cell_numel(input):
 	return out
 
 def cell_2_vec(input):
+	if len(input.shape) > 2:
+		raise ValueError("Expected a 2-d array (cell) for an input.")
 	cn = cell_numel(input)
-	cs = np.cumsum(input)
-	starts = np.array(list([1]) + list(cs[0:cs.size - 1] + 1))
-	end = cs
+	cs = []
+	for i in np.cumsum(input):
+		cs.append(np.sum(i))
+	cs = np.array(cs)
+	starts, ends = [], []
+	start_counter = 0
+	for i in input:
+		starts.append(start_counter)
+		ends.append(start_counter + len(i) - 1)
+		start_counter += len(i)
+	starts, ends = np.array(starts), np.array(ends)
 	vec = []
-	for i in range(input.size):
-		vec[starts[i]:ends[i]] = input[i]
+	for i in input:
+		vec += i
 	vec = np.array(vec)
 	return (starts, ends, cn, vec)
