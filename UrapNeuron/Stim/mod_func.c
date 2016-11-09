59e080568115343b4cfb7754f0975d6e301ea9ef
roybens <bens.roy@gmail.com>
2016-11-09 11:42:40 -0800

    adding stim

---
+++ UrapNeuron/Stim/mod_func.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "hocdec.h"
+#define IMPORT extern __declspec(dllimport)
+IMPORT int nrnmpi_myid, nrn_nobanner_;
+
+extern void _branching_reg();
+extern void _hh2_reg();
+extern void _stim_reg();
+
+modl_reg(){
+	//nrn_mswindll_stdio(stdin, stdout, stderr);
+    if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
+	fprintf(stderr, "Additional mechanisms from files\n");
+
+fprintf(stderr," branching.mod");
+fprintf(stderr," hh2.mod");
+fprintf(stderr," stim.mod");
+fprintf(stderr, "\n");
+    }
+_branching_reg();
+_hh2_reg();
+_stim_reg();
+}
